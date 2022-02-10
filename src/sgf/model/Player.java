9b28f714b39cca858ace5ec512ac740af2aee41d
Luca <luca.venturi29@studio.unibo.it>
2022-02-10 16:34:30 +0100

    Create Player interface (Testing branching)

---
+++ src/sgf/model/Player.java
@@ -0,0 +1,16 @@
+package sgf.model;
+
+/**
+ * 
+ *
+ */
+public interface Player {
+    /**
+     * @return Player current HP.
+     */
+    int getCurrentHP();
+    /**
+     * @return Player current HP.
+     */
+    int getCurrentMoney();
+}
