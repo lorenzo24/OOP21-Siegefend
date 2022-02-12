8b1abd98f942d6b908bf55b66830192522c6edd8
Fabio <fabio.notaro2@studio.unibo.it>
2022-02-12 15:43:02 +0100

    Create enum TileType

---
+++ src/sgf/model/TileType.java
@@ -0,0 +1,26 @@
+package sgf.model;
+/**
+ * 
+ * Describes the various and different types of a {@link Tile} in the {@link Map}.
+ *
+ */
+public enum TileType {
+    /**
+     * This type denotes that the {@link Tile} is the beginning of the path, that is the
+     * tile from which the enemies will appear.
+     */
+    START_PATH,
+    /**
+     * This type denotes that the {@link Tile} composes the path.
+     */
+    PATH,
+    /**
+     * This type denotes that the {@link Tile} represents the end of the path, that is 
+     * the tile in which the enemies will disappear.
+     */
+    END_PATH,
+    /**
+     * This type denotes that the {@link Tile} doesn't compose the path.
+     */
+    OTHER
+}
