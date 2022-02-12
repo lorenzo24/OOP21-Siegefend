c55f2e0f06daba83f8ccddc3cb7600c2d917c438
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2022-02-12 17:20:02 +0100

    Creation of the getMap method in the level interface

--- src/sgf/model/Level.java
+++ src/sgf/model/Level.java
@@ -1,14 +1,13 @@
 package sgf.model;
 
 /**
- * 
- * 
- *
+ * Represents a level of play.
  */
 public interface Level {
 
     /**
-     * 
+     * Method that returns the {@link Map} of the level.
+     * @return the level's {@link Map}.
      */
-    void test();
+    Map getMap();
 }
