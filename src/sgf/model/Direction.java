f80d0a6b1a442cf2071282877b798f0a25d38f1f
Fabio <fabio.notaro2@studio.unibo.it>
2022-02-12 15:49:36 +0100

    Create enum Direction

---
+++ src/sgf/model/Direction.java
@@ -0,0 +1,25 @@
+package sgf.model;
+/**
+ * 
+ * This enumeration represents a simple direction, that will be useful in order to simplify
+ * enemies' movement along the path.
+ *
+ */
+public enum Direction {
+    /**
+     * Left direction.
+     */
+    LEFT,
+    /**
+     * Right direction.
+     */
+    RIGHT,
+    /**
+     * Up direction.
+     */
+    UP,
+    /**
+     * Down direction.
+     */
+    DOWN
+}
