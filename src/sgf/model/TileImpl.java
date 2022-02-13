f6558fd6a826e8d0f3dc65efa81a70e665930573
Fabio <fabio.notaro2@studio.unibo.it>
2022-02-13 16:43:44 +0100

    Create class TileImpl

---
+++ src/sgf/model/TileImpl.java
@@ -0,0 +1,45 @@
+package sgf.model;
+
+import java.util.Optional;
+/**
+ * 
+ * Class representing the implementation a {@link Map}'s {@link Tile}.
+ *
+ */
+public class TileImpl implements Tile {
+    private final TileType tileType;
+    private final Position position;
+    private final Optional<Direction> tileDirection;
+    
+    /**
+     * Constructor for the creation of a {@link Tile}.
+     * @param tileType Is the type that describes the tile.
+     * @param position Is the tile's {@link Position} in the {@link Map}. 
+     * @param tileDirection Describes in which {@link Direction} must proceed the {@link Enemy}'s movement.
+     */
+    public TileImpl(final TileType tileType, final Position position, final Optional<Direction> tileDirection) {
+        this.tileType = tileType;
+        this.position = position;
+        this.tileDirection = tileDirection;
+    }
+
+    @Override
+    public TileType getTileType() {
+        return this.tileType;
+    }
+
+    @Override
+    public Optional<Direction> getTileDirection() {
+        return this.tileDirection;
+    }
+
+    @Override
+    public Position getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public boolean canContainTurret() {
+        return this.tileType == TileType.OTHER;
+    }
+}
