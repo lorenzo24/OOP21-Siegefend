ebed50fea436cac12907af81aea262e50621120b
Fabio <fabio.notaro2@studio.unibo.it>
2022-02-14 17:53:54 +0100

    Create simple example of GUI with adaptive size

---
+++ src/sgf/controller/StartGame.java
@@ -0,0 +1,17 @@
+package sgf.controller;
+
+import sgf.view.ScreenGame;
+
+/**
+ * 
+ * Class which goal is to start the game.
+ *
+ */
+public class StartGame {
+    /**
+     * Starting point for the game.
+     */
+    public static void main(String[] args) {
+       ScreenGame screen = new ScreenGame();
+    }
+}
---
+++ src/sgf/view/ScreenGame.java
@@ -0,0 +1,42 @@
+package sgf.view;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.*;
+import java.awt.event.*;
+/**
+ * 
+ * This class represent a simple screen game.
+ *
+ */
+public class ScreenGame {
+    private static final double WIDTH_PERC = 0.5;
+    private static final double HEIGHT_PERC = 0.5;
+    
+    private final JFrame frame = new JFrame();
+    private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    
+    /**
+     * Constructor.
+     */
+    public ScreenGame() {
+        frame.setTitle("SIEGEFEND");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        frame.setVisible(true);
+        this.windowClosing();
+    }
+    
+    private void windowClosing() {
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(final WindowEvent e) {
+                final int choise = JOptionPane.showConfirmDialog(frame, 
+                        "Do you really want to quit?", "QUITTING", JOptionPane.YES_NO_OPTION);
+                if (choise == JOptionPane.YES_OPTION) {
+                    System.exit(0);     // Find an alternative to System.exit.
+                }
+            }
+        });
+    }
+}
