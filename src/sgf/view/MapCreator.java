3c9e88fdfde26ce780e181d5136b7de5d7495b24
Fabio <fabio.notaro2@studio.unibo.it>
2022-02-15 18:59:00 +0100

    Add random image choise to fill the screen and continue update in class MapCreator

--- src/sgf/view/MapCreator.java
+++ src/sgf/view/MapCreator.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Random;
+
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 /**
@@ -17,13 +19,18 @@ public class MapCreator extends JPanel {
     private static final long serialVersionUID = -7141712951441617040L;
     private static final int MATRIX_SIZE = 20;
     private BufferedImage grassImage;
+    private BufferedImage waterImage;
+    private BufferedImage sandImage;
+    private BufferedImage[] imagesArray = new BufferedImage[3];
 
     /**
      * Constructor for a map creator.
      */
     public MapCreator() {
         try {
-            grassImage = ImageIO.read(new FileInputStream("res" + File.separator + "grass.png"));
+            imagesArray[0] = ImageIO.read(new FileInputStream("res" + File.separator + "grass.png"));
+            imagesArray[1] = ImageIO.read(new FileInputStream("res" + File.separator + "sand.png"));
+            imagesArray[2] = ImageIO.read(new FileInputStream("res" + File.separator + "water.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,13 +47,16 @@ public class MapCreator extends JPanel {
         final Image modifiedImage = this.dinamicResize(widthImage, heightImage);
         for (int x = 0; x < MATRIX_SIZE; x++) {
             for (int y = 0; y < MATRIX_SIZE; y++) {
+                
                 graphic.drawImage(modifiedImage, x * widthImage, y * heightImage, null);
             }
         }
+        repaint();
     }
 
     private Image dinamicResize(final int widthImage, final int heightImage) {
-        return  grassImage.getScaledInstance(widthImage, heightImage, Image.SCALE_SMOOTH);
+        Random r = new Random();
+        return imagesArray[r.nextInt(3)].getScaledInstance(widthImage, heightImage, Image.SCALE_SMOOTH);
     }
 
     private int sizeImage(final double dimension) {
