848a7f763bb7259113e71dea2b233ed91279d587
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2022-02-15 17:33:37 +0100

    Create a dynamic sized matrix of images

--- src/sgf/view/MapCreator.java
+++ src/sgf/view/MapCreator.java
@@ -1,6 +1,7 @@
 package sgf.view;
 
 import java.awt.image.BufferedImage;
+import java.awt.Image;
 import java.awt.Graphics;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,8 +14,10 @@ import javax.swing.JPanel;
  *
  */
 public class MapCreator extends JPanel {
+    private static final int NUMBER_OF_CELLS = 25;
     private static final long serialVersionUID = -7141712951441617040L;
     private BufferedImage grassImage;
+
     /**
      * Constructor for a map creator.
      */
@@ -31,7 +34,20 @@ public class MapCreator extends JPanel {
      */
     public void paintComponent(final Graphics graphic) {
         super.paintComponent(graphic);
-        graphic.drawImage(grassImage, 0, 0, null);
+        final int widthImage = sizeImage(this.getWidth());
+        final int heightImage =  sizeImage(this.getHeight());
+        final Image modifiedImage = this.dinamicResize(widthImage, heightImage);
+        for (int x = 0; x < NUMBER_OF_CELLS; x++) {
+            for (int y = 0; y < NUMBER_OF_CELLS; y++) {
+                graphic.drawImage(modifiedImage, x * widthImage, y * heightImage, null);
+            }
+        }
     }
 
+    private Image dinamicResize(final int widthImage, final int heightImage) {
+        return  grassImage.getScaledInstance(widthImage, heightImage, Image.SCALE_SMOOTH);
+    }
+    private int sizeImage(final double dimension) {
+        return (int) Math.round(dimension / NUMBER_OF_CELLS);
+    }
 }
