438f044d6b1e29208a2745e18abc7172014081e8
Leo <leogiaco2@gmail.com>
2022-02-15 20:02:18 +0100

    Create test views for the shop
    All the commented code can be used to see how the shop would appear

--- src/sgf/view/ShopTurretInfo.java
+++ src/sgf/view/ShopTurretInfo.java
@@ -1,18 +1,59 @@
 package sgf.view;
 
+import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import sgf.model.Turret;
 
+/**
+ *
+ */
 public class ShopTurretInfo extends JPanel {
 
-    private Turret turret;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private JLabel turretImg;
+    private final JButton turretBuy;
+    private final JLabel turretPrice;
+
+    private ShopTurretInfo() {
+        this.turretImg = new JLabel();
+        this.turretPrice = new JLabel("Failed to load turret");
+        this.turretBuy = new JButton("");
+        this.turretBuy.setEnabled(false);
+    }
 
     private ShopTurretInfo(final Turret t) {
-        this.turret = t;
+        try {
+            this.turretImg = new JLabel(new ImageIcon(ImageIO.read(new File("res/test/vuoto.png"))));
+        } catch (IOException e) {
+            this.turretImg = new JLabel("No Image Found");
+        }
+        this.turretBuy = new JButton("Buy");
+        this.turretPrice = new JLabel(Integer.toString(t.getPrice()));
+        /*
+         * 
+         */
+        final JPanel panel = new JPanel(new BorderLayout());
+        final JPanel intPanel = new JPanel(new BorderLayout());
+        panel.add(turretImg, BorderLayout.CENTER);
+        intPanel.add(turretPrice, BorderLayout.CENTER);
+        intPanel.add(turretBuy, BorderLayout.SOUTH);
+        panel.add(intPanel, BorderLayout.SOUTH);
+        this.add(panel);
     }
 
     static ShopTurretInfo from(final Turret t) {
-        return new ShopTurretInfo();
+        return new ShopTurretInfo(t);
     }
 }
--- src/sgf/view/ShopView.java
+++ src/sgf/view/ShopView.java
@@ -1,13 +1,113 @@
 package sgf.view;
 
-import javax.swing.JButton;
+//import java.awt.Dimension;
+import java.awt.FlowLayout;
+//import java.awt.Toolkit;
+import java.util.List;
+//import java.util.Optional;
+import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+//
+//import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ShopView extends JPanel {
+//import sgf.model.Enemy;
+//import sgf.model.Position;
+import sgf.model.Turret;
 
+/**
+ *
+ */
+public class ShopView extends JPanel /* JFrame */ {
 
-    public ShopView() {
-        // TODO Auto-generated constructor stub
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+//    private static final double WIDTH_PERC = 0.4;
+//    private static final double HEIGHT_PERC = 0.4;
+    private List<ShopTurretInfo> turretInfo;
+
+    /**
+     * 
+     * @param turrets
+     */
+    public ShopView(final List<Turret> turrets) {
+//        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+//        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+//        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.turretInfo = turrets.stream().map(t -> ShopTurretInfo.from(t)).collect(Collectors.toList());
+        final JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        this.turretInfo.forEach((t) -> panel.add(t));
+        this/*.getContentPane()*/.add(panel);
+
+//        this.setVisible(true);
     }
 
+/*
+    public static void main(final String... args) {
+        List<Turret> turrets = Stream.iterate(0, i -> i + 1)
+                            .limit(10)
+                            .map(i -> new Turret() {
+                                @Override
+                                public boolean isAttacking() {
+                                    return false;
+                                }
+
+                                @Override
+                                public void idle() {
+                                }
+
+                                @Override
+                                public Optional<Enemy> getTarget() {
+                                    return null;
+                                }
+
+                                @Override
+                                public double getRange() {
+                                    return 0;
+                                }
+
+                                @Override
+                                public int getPrice() {
+                                    return i * 100;
+                                }
+
+                                @Override
+                                public Position getPosition() {
+                                    return null;
+                                }
+
+                                @Override
+                                public int getID() {
+                                    return 0;
+                                }
+
+                                @Override
+                                public double getFireRate() {
+                                    return 0;
+                                }
+
+                                @Override
+                                public void fireAt(Position target) {
+                                }
+
+                                @Override
+                                public Optional<Enemy> findTarget() {
+                                    return null;
+                                }
+
+                                @Override
+                                public void attack() {
+                                }
+
+                                @Override
+                                public Turret clone() throws CloneNotSupportedException {
+                                    return (Turret) super.clone();
+                                }
+                            }).collect(Collectors.toList());
+        new ShopView(turrets);
+    } 
+*/
 }
