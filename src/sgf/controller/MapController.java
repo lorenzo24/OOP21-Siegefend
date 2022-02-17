8a857e95c5e620b51099eac257751bbfe2da69f8
Fabio <fabio.notaro2@studio.unibo.it>
2022-02-17 10:45:09 +0100

    Clean code in class MapController

--- src/sgf/controller/MapController.java
+++ src/sgf/controller/MapController.java
@@ -1,41 +1,43 @@
 package sgf.controller;
 
+import sgf.model.Map;
+import sgf.model.MapImpl;
 import sgf.view.ScreenGame;
 
 /**
- * This class is responsible for the map's multithreading management.
+ * This class is responsible for the map's multithreading management and also to show it.
  *
  */
 public class MapController {
-    private static final int SLEEP_TIME = 50;   // Modify this value in order to make the animation slower or faster.
-    private static final int A_SECOND_IN_MILLIS = 1000;
+    private static final int SLEEP_TIME = 100;   // Modify this value in order to make the animation slower or faster.
     private final ScreenGame screen;
-    private final TileController tileController;
+    private final Map map = new MapImpl();
 
     /**
-     * Constructor that also start thread loop.
+     * Constructor that sets up the screen and also start thread loop.
      */
     public MapController() {
         screen = new ScreenGame();
-        this.tileController = new TileController();
-        this.startGameThread();
+        this.startMapThread();
     }
 
-    private void startGameThread() {
+    private void startMapThread() {
         final Thread gameThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 while (true) {
                     try {
-                        screen.getMapCreator().showGridImage();
+                        // Taken the input matrix, show the correspondent map (grid of tiles), by
+                        // first creating an array of tile from which load the images.
+                        screen.getMapCreator().showGridImage(map.getMapComposition());
                         Thread.sleep(SLEEP_TIME);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    System.out.print(A_SECOND_IN_MILLIS / SLEEP_TIME + " fps\n");
                 }
             }
         });
         gameThread.start();
     }
+    // TODO Show fps in old way and reload image every x seconds to avoid "sgranatura".
 }
--- src/sgf/view/MapCreator.java
+++ src/sgf/view/MapCreator.java
@@ -1,58 +1,63 @@
 package sgf.view;
 
 import java.awt.image.BufferedImage;
+
 import java.awt.Image;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Random;
-import javax.imageio.ImageIO;
+import java.awt.event.*;
 import javax.swing.JPanel;
+
+import sgf.controller.TileController;
 
 /**
  * 
  * This class is responsible for the process of map's creation and showing.
  *
  */
-public class MapCreator extends JPanel {
+public class MapCreator extends JPanel implements ComponentListener{
     private static final long serialVersionUID = -7141712951441617040L;
     private static final int MATRIX_SIZE = 20;
-    private BufferedImage[] imagesArray = new BufferedImage[3];
-
-    /**
-     * Constructor for a map creator. It also fill an array with all images in res folder.
-     */
+    private final TileController tileController = new TileController();
+    private int[][] internalMatrix;
     public MapCreator() {
-        try {
-            imagesArray[0] = ImageIO.read(new FileInputStream("res" + File.separator + "grass.png"));
-            imagesArray[1] = ImageIO.read(new FileInputStream("res" + File.separator + "sand.png"));
-            imagesArray[2] = ImageIO.read(new FileInputStream("res" + File.separator + "water.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.addComponentListener(this);
     }
-
     /**
      * Method that loads and show a random image from the array of images.
      * 
      */
-    public void showGridImage() {
+    public void showGridImage(int[][] mapToBeShowed) {
+      
         final int widthImage = this.sizeImage(this.getWidth());
         final int heightImage =  this.sizeImage(this.getHeight());
-        final Image modifiedImage = this.dinamicResize(widthImage, heightImage);
+        
         for (int x = 0; x < MATRIX_SIZE; x++) {
             for (int y = 0; y < MATRIX_SIZE; y++) {
-                this.getGraphics().drawImage(modifiedImage, x * widthImage, y * heightImage, null);
+                final Image img = tileController.getImage(mapToBeShowed[y][x]);
+                this.getGraphics().drawImage(img, x * widthImage, y * heightImage, null);
             }
         }
     }
-
-    private Image dinamicResize(final int widthImage, final int heightImage) {
-        final Random r = new Random();
-        return imagesArray[r.nextInt(3)].getScaledInstance(widthImage, heightImage, Image.SCALE_SMOOTH);
+    public void componentResized(ComponentEvent e) {
+        this.tileController.correctImagesSize(this.sizeImage(this.getWidth()), this.sizeImage(this.getHeight()));
+        
     }
 
     private int sizeImage(final double dimension) {
         return (int) Math.round(dimension / MATRIX_SIZE);
+    }
+    @Override
+    public void componentMoved(ComponentEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void componentShown(ComponentEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void componentHidden(ComponentEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
