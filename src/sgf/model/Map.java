02c84d5a69fecda1b1ab5dab2acf834e95bfcf97
Fabio <fabio.notaro2@studio.unibo.it>
2022-02-17 11:49:51 +0100

    Clean code and update comments in class MapImpl

--- src/sgf/model/Map.java
+++ src/sgf/model/Map.java
@@ -11,10 +11,17 @@ public interface Map {
      * @return the tile from the given grid position.
      */
     Tile getTileFromGridPosition(GridPosition position);
+
     /**
      * This method helps getting a {@link Tile} from a given {@link Position}.
      * @param position Is the position of the interested {@link Tile}.
      * @return the tile from the given {@link Position}.
      */
     Tile getTileFromPosition(Position position);
+
+    /**
+     * This method returns the 2D array that represents the map.
+     * @return a 2D array which is the tiles grid.
+     */
+    int[][] getMapComposition();
 }
--- src/sgf/model/MapImpl.java
+++ src/sgf/model/MapImpl.java
@@ -1,12 +1,38 @@
 package sgf.model;
+
+import java.util.Arrays;
+
 /**
- * 
- * This class represents a simple map implementation.
- *
+ * This class represents a simple map implementation with its structure.
  */
 public class MapImpl implements Map {
+    // TODO Maybe this field can be removed and only used a 2d array.
     private java.util.Map<GridPosition, Tile> tiles;
-    
+
+    // Value 0 is grass, value 1 is path and value 2 is water.
+    private final int[][] basicMap = {
+            { 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+          };
+
     @Override
     public Tile getTileFromGridPosition(final GridPosition position) {
         return this.tiles.get(position);
@@ -16,6 +42,11 @@ public class MapImpl implements Map {
     public Tile getTileFromPosition(final Position position) {
         // TODO Auto-generated method stub
         return null;
+    }
+    
+    @Override
+    public int[][] getMapComposition() {
+        return Arrays.copyOf(this.basicMap, this.basicMap.length);      // Returns a defensive copy.
     }
 
 }
