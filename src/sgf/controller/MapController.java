e3c7f764b55889599330274089f0a9bcbd69d236
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2022-02-24 23:16:35 +0100

    Make enemy and map controller deprecated

--- src/sgf/controller/MapController.java
+++ src/sgf/controller/MapController.java
@@ -3,34 +3,22 @@ package sgf.controller;
 import sgf.model.GridPosition;
 import sgf.model.Map;
 import sgf.model.MapImpl;
-import sgf.view.MapCreator;
+import sgf.model.Tile;
+import sgf.view.MapView;
 import sgf.view.ScreenGame;
 
 /**
  * This class is responsible for the map's multithreading management and also to show it.
  */
 public class MapController {
-    private static final int SLEEP_TIME = 10;   // Modify this value in order to make the animation slower or faster.
-    private final ScreenGame screen;    // View field.
     private final Map map;      // Model field.
-    private volatile boolean threadRun = true;
     /**
      * Constructor that sets up the screen and also start thread loop.
      */
     public MapController() {
-        this.map = new MapImpl(1);      // 1 denotes to create the map for the first level.
-        screen = new ScreenGame(this.map);      // The map is passed in order to create a MapCreator into ScreenGame.
         this.obtainTileFromMouseClick();        // Method that return the correspondent Tile of a click on the screen.
-        this.startMapThread();
     }
 
-    /**
-     * Take the JPannel from the view.
-     * @return the JPannel of the view 
-     */
-    public MapCreator getjPannel() {
-        return screen.getMapCreator();
-    }
 
     /**
      * Method that calculates the tile clicked on the screen.
@@ -51,51 +39,5 @@ public class MapController {
     private int convertCoordinate(final int x, final double dimension) {
         final double sizeOfASingleTile = dimension / screen.getMapCreator().getMatrixSize();
         return (int) (x / sizeOfASingleTile);
-    }
-
-    private void startMapThread() {
-        final Thread gameThread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                screen.getMapCreator().createMapImage();
-                int ups = 0;
-                long lastTime = System.currentTimeMillis();
-                while (threadRun) {
-                    // Print how many update has been done in 1 second.
-                    if (System.currentTimeMillis() - lastTime >= 1000) {
-                        System.out.print("UPS:" + ups + "\n");
-                        ups = 0;
-                        lastTime = System.currentTimeMillis();
-                    }
-                    try {
-                        // TODO Deleting this if problems related to hiding windows disappears. Maybe this if must be deleted? 
-                        if (screen.getMapCreator().isUpdateNeeded()) {
-                            screen.getMapCreator().showGridImage();
-                        }
-                        ups++;
-                        Thread.sleep(SLEEP_TIME);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        gameThread.start();
-    }
-    
-    // TODO pause and resume thread must change following game status.
-    /**
-     * Temporary stops the thread.
-     */
-    public void pauseMapThread() {
-        this.threadRun = false;
-    }
-
-    /**
-     * Resume the thread.
-     */
-    public void resumeMapThread() {
-        this.threadRun = true;
     }
 }
