fec607f3d7a8645e8564c324c0d1a974cfb27688
Leo <leogiaco2@gmail.com>
2022-02-26 22:21:34 +0100

    Add variable in AppStart::main

--- src/sgf/utilities/AppStart.java
+++ src/sgf/utilities/AppStart.java
@@ -13,7 +13,8 @@ public final class AppStart {
      * @param args
      */
     public static void main(final String[] args) {
-
+        GameManager g = null;
+        //GameManager g = new GameManagerImpl();
     }
 
 }
