1c2e5225a83c3410b7e906fabba6c35bc0a07eca
Leo <leogiaco2@gmail.com>
2022-03-02 22:47:14 +0100

    Remove/Substitute imports

--- src/sgf/utilities/AppStart.java
+++ src/sgf/utilities/AppStart.java
@@ -1,16 +1,36 @@
 package sgf.utilities;
 
+import sgf.controller.PlayingController;
+import sgf.controller.PlayingControllerImpl;
+import sgf.controller.ShopController;
+import sgf.controller.ShopControllerImpl;
 import sgf.controller.map.MapController;
+import sgf.controller.map.MapControllerImpl;
 import sgf.controller.thread.GameController;
+import sgf.controller.thread.GameControllerImpl;
+import sgf.model.Map;
+import sgf.view.AbstractEnemyView;
+import sgf.view.AbstractGameView;
+import sgf.view.AbstractMapView;
+import sgf.view.AbstractPlayingView;
+import sgf.view.AbstractShopView;
 import sgf.view.EnemyController;
 import sgf.view.EnemyView;
+import sgf.view.EnemyViewImpl;
 import sgf.view.GameView;
+import sgf.view.GameViewImpl;
 import sgf.view.MapView;
+import sgf.view.MapViewImpl;
+import sgf.view.PlayingViewImpl;
+import sgf.view.ScreenGame;
+import sgf.view.ShopViewImpl;
 
 /**
  *
  */
 public final class AppStart {
+
+    private static final int CELL_SIZE = 80;
 
     private AppStart() {
     }
@@ -23,12 +43,25 @@ public final class AppStart {
         final GameManager gameManager = null;
         final PlayerManager playerManager = null;
         final LevelManager levelManager = null;
-        final GameController gameController = null;
-        final GameView gameView = null;
-        final MapController mapController = null;
-        final MapView mapView = null;
-        final EnemyController enemyController = null;
-        final EnemyView enemyView = null;
+
+        final MapLoader mapLoader = null;       // TODO: urgent
+        final Map m = mapLoader.getMap();
+
+        /*
+         * At the start only the menu, settings and levels view will be created.
+         * All these other views and controllers will be created when someone clicks on a level.
+         */
+
+        final MapController mapController = new MapControllerImpl();
+        final AbstractMapView mapView = new MapViewImpl(m, CELL_SIZE);
+        final EnemyController enemyController = null;   // TODO: urgent
+        final AbstractEnemyView enemyView = new EnemyViewImpl(m.getMapSize(), CELL_SIZE);
+        final GameController gameController = new GameControllerImpl();
+        final AbstractGameView gameView = new GameViewImpl(mapView, enemyView);
+        final ShopController shopController = new ShopControllerImpl(gameManager);
+        final AbstractShopView shopView = new ShopViewImpl(gameManager);
+        final PlayingController playingController = new PlayingControllerImpl(gameManager, playerManager);
+        final AbstractPlayingView playingView = new PlayingViewImpl(gameView, shopView);
         /**
          * Linking.
          */
@@ -38,5 +71,11 @@ public final class AppStart {
         mapView.setController(mapController);
         enemyController.setView(enemyView);
         enemyView.setController(enemyController);
+        shopController.setView(shopView);
+        shopView.setController(shopController);
+        playingController.setView(playingView);
+        playingView.setController(playingController);
+
+        new ScreenGame(playingView);
     }
 }
