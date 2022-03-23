b848c0841d0ae230b4db192e75fe9d54044ac855
Fabio <fabio.notaro2@studio.unibo.it>
2022-03-23 23:02:01 +0100

    Improve music management in MusicControllerImpl

--- src/sgf/controller/game/MusicControllerImpl.java
+++ src/sgf/controller/game/MusicControllerImpl.java
@@ -1,10 +1,13 @@
 package sgf.controller.game;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * Controller for the music that manages the tracks random choice.
@@ -25,27 +28,27 @@ public class MusicControllerImpl implements MusicController {
 
     @Override
     public void musicOn() {
+        this.c.start();
         this.musicOn = true;
     }
 
     @Override
     public void musicOff() {
         this.musicOn = false;
+        this.c.stop();
     }
 
     // This method open an audio stream and start the music file.
     private void play(final String fileName) {
         final String musicFile = "res" + File.separator + fileName + ".wav";
         final File file = new File(musicFile);
-        try {
-            final AudioInputStream a = AudioSystem.getAudioInputStream(file);
-            c = AudioSystem.getClip();
-            c.open(a);
-            c.start();
-            a.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } 
+            try (AudioInputStream a = AudioSystem.getAudioInputStream(file)) {
+                c = AudioSystem.getClip();
+                c.open(a);
+                c.start();
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                e.printStackTrace();
+            }
     }
 
     // Thread that plays and changes music while music is on.
