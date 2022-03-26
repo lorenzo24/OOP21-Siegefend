43b01ec3aa4649550281b53b1aa65cfd5b67fd43
Fabio <fabio.notaro2@studio.unibo.it>
2022-03-26 15:37:46 +0100

    Change time to check when a music is over in MusicControllerImpl

--- src/sgf/controller/game/MusicControllerImpl.java
+++ src/sgf/controller/game/MusicControllerImpl.java
@@ -14,6 +14,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 public class MusicControllerImpl implements MusicController {
     private static final int SONGS_NUMBER = 3;
+    private static final int  DURATION = 3000;
     private boolean musicOn;
     private Clip c;
 
@@ -61,7 +62,7 @@ public class MusicControllerImpl implements MusicController {
                         changeMusic();
                     }
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(DURATION);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
