40172f99009cc0df813f9614cd90873f38e96c1a
Leo <leogiaco2@gmail.com>
2022-04-04 22:47:16 +0200

    Remove unused import

--- src/sgf/view/shop/ShopViewImpl.java
+++ src/sgf/view/shop/ShopViewImpl.java
@@ -2,7 +2,6 @@ package sgf.view.shop;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
