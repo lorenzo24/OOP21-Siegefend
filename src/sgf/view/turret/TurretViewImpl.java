87fada3f81c0a9523d7bba34ef4b249d30a004eb
Leo <leogiaco2@gmail.com>
2022-04-13 23:14:59 +0200

    Implement MouseListener, implement buying and placing turrets

--- src/sgf/view/turret/TurretViewImpl.java
+++ src/sgf/view/turret/TurretViewImpl.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.Semaphore;
@@ -13,6 +15,7 @@ import javax.swing.SwingUtilities;
 import sgf.controller.turret.TurretController;
 import sgf.helpers.ImgTileSize;
 import sgf.managers.TurretImageManager;
+import sgf.model.map.GridPosition;
 import sgf.model.map.Position;
 import sgf.utilities.LockClass;
 import sgf.utilities.PositionConverter;
@@ -22,13 +25,12 @@ import sgf.utilities.PositionConverter;
  * 
  *
  */
-public class TurretViewImpl extends AbstractTurretView {
-
-
+public class TurretViewImpl extends AbstractTurretView implements MouseListener {
     private boolean isControllerSet;
     private TurretController turretController;
     private boolean ready;
     private final BufferedImage image;
+    private final int matrixSize;
     private final int tileSize;
     private final Semaphore semaphore;
     private final TurretImageManager imgManager;
@@ -41,6 +43,7 @@ public class TurretViewImpl extends AbstractTurretView {
      */
     public TurretViewImpl(final int matrixSize, final Semaphore semaphore) {
         this.setVisible(false);
+        this.matrixSize = matrixSize;
         this.tileSize = ImgTileSize.getTileSize();
         this.image = new BufferedImage(matrixSize * this.tileSize, matrixSize * this.tileSize, BufferedImage.TYPE_INT_ARGB);
         this.semaphore = semaphore;
@@ -61,6 +64,7 @@ public class TurretViewImpl extends AbstractTurretView {
         if (this.isControllerSet) {
             this.ready = true;
             this.setVisible(true);
+            this.addMouseListener(this);
         } else {
             throw new IllegalStateException("Cannot invoke start() if the controller has not been set.");
         }
@@ -91,14 +95,12 @@ public class TurretViewImpl extends AbstractTurretView {
             final var entry = iterator.next();
             final int entryID = entry.getValue().getID();
             final Position p = entry.getValue().getPosition();
-            int origW = imgManager.getImage(entryID).getWidth(null);
-            int origH = imgManager.getImage(entryID).getHeight(null);
+            final int origW = imgManager.getImage(entryID).getWidth(null);
+            final int origH = imgManager.getImage(entryID).getHeight(null);
             entry.getValue().setAngle(entry.getValue().getAngle() + 0.1); // test
-            BufferedImage bimg = rotateImage(imgManager.getImage(entryID), entry.getValue().getAngle());
-            double scaleX = (double) bimg.getWidth() / origW;
-            double scaleY = (double) bimg.getHeight() / origH;
-            System.out.println("Scala X:" + scaleX);
-            System.out.println("Scala Y:" + scaleY);
+            final BufferedImage bimg = rotateImage(imgManager.getImage(entryID), entry.getValue().getAngle());
+            final double scaleX = (double) bimg.getWidth() / origW;
+            final double scaleY = (double) bimg.getHeight() / origH;
             gImage.drawImage(bimg, (int) (p.getX() - (this.tileSize * (scaleX - 1) / 2)), (int) (p.getY() - (this.tileSize * (scaleY - 1) / 2)), (int) (this.tileSize * scaleX), (int) (this.tileSize * scaleY), null);
         }
         semaphore.release();
@@ -111,11 +113,11 @@ public class TurretViewImpl extends AbstractTurretView {
      * @return
      */
     public static BufferedImage rotateImage(final Image img, final double rads) {
-        double sin = Math.abs(Math.sin(rads)), cos = Math.abs(Math.cos(rads));
-        int w = img.getWidth(null);
-        int h = img.getHeight(null);
-        int newWidth = (int) Math.floor(w * cos + h * sin);
-        int newHeight = (int) Math.floor(h * cos + w * sin);
+        final double sin = Math.abs(Math.sin(rads)), cos = Math.abs(Math.cos(rads));
+        final int w = img.getWidth(null);
+        final int h = img.getHeight(null);
+        final int newWidth = (int) Math.floor(w * cos + h * sin);
+        final int newHeight = (int) Math.floor(h * cos + w * sin);
 
         /* Debug Only
         System.out.println("new width: " + newWidth);
@@ -124,9 +126,9 @@ public class TurretViewImpl extends AbstractTurretView {
         System.out.println("orig heigth: " + h);
         */
 
-        BufferedImage rotated = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = rotated.createGraphics();
-        AffineTransform at = new AffineTransform();
+        final BufferedImage rotated = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+        final Graphics2D g2d = rotated.createGraphics();
+        final AffineTransform at = new AffineTransform();
         at.translate((newWidth - w) / 2, (newHeight - h) / 2);
 
         final int x = w / 2;
@@ -139,5 +141,33 @@ public class TurretViewImpl extends AbstractTurretView {
 
         return rotated;
     }
+
+    @Override
+    public void mouseClicked(final MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            //this.mouseHandler.accept(e);
+            final int gridColumn = this.convertCoordinate(e.getX(), this.getWidth());
+            final int gridRow = this.convertCoordinate(e.getY(), this.getHeight());
+            this.turretController.addSelectedTurret(new GridPosition(gridRow, gridColumn));
+        }
+    }
+
+    // Taken a value and the dimension in refers to, it returns an integer value that is the corresponding tile position in the dimension.
+    private int convertCoordinate(final int x, final double dimension) {
+        final double sizeOfASingleTile = dimension / this.matrixSize;
+        return (int) (x / sizeOfASingleTile);
+    }
+
+    @Override
+    public void mousePressed(final MouseEvent e) { }
+
+    @Override
+    public void mouseReleased(final MouseEvent e) { }
+
+    @Override
+    public void mouseEntered(final MouseEvent e) { }
+
+    @Override
+    public void mouseExited(final MouseEvent e) { }
 
 }
