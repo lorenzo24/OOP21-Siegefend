325a1b9f08dfbabd65694838e6bd44999ec3fd25
Leo <leogiaco2@gmail.com>
2022-04-13 20:13:24 +0200

    Remove unused methods from interface

--- src/sgf/managers/GameManager.java
+++ src/sgf/managers/GameManager.java
@@ -5,7 +5,6 @@ import sgf.model.game.GameStatus;
 import sgf.model.game.Pausable;
 import sgf.model.level.Level;
 import sgf.model.level.Wave;
-import sgf.model.turret.Turret;
 
 /**
  * Handles the coordination of the various game components.
@@ -69,18 +68,4 @@ public interface GameManager {
      * @return the {@link GameStatus} of the game
      */
     GameStatus getCurrentStatus();
-
-    /**
-     * Purchases a turret and updates the player's money count.
-     * @param t The turret to purchase
-     * @return {@code true} if the purchase is successful, {@code false} otherwise
-     */
-    boolean purchase(Turret t);
-
-    /**
-     * Purchases an upgrade for the given turret and updates the player's money count.
-     * @param t The turret to upgrade
-     * @return {@code true} if the purchase is successful, {@code false} otherwise
-     */
-    boolean purchaseUpgrade(Turret t);
 }
