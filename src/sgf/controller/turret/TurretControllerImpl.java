08fdaa2738569cb07feeb4bcb046cb66f4bf2378
Lorenzo <lorenzo.gessi2@gmail.com>
2022-04-14 17:17:43 +0200

    Replace Turret with TurretManager and Adapt methods to the change, Add EnemyController and GameManager

--- src/sgf/controller/turret/TurretControllerImpl.java
+++ src/sgf/controller/turret/TurretControllerImpl.java
@@ -6,8 +6,12 @@ import java.util.Map.Entry;
 import java.util.concurrent.Semaphore;
 import java.util.Optional;
 
+import sgf.controller.enemy.EnemyController;
 import sgf.controller.shop.ShopController;
 import sgf.helpers.ImgTileSize;
+import sgf.managers.GameManager;
+import sgf.managers.TurretManager;
+import sgf.managers.TurretManagerImpl;
 import sgf.model.map.GridPosition;
 import sgf.model.map.Map;
 import sgf.model.turret.Turret;
@@ -26,22 +30,28 @@ public class TurretControllerImpl implements TurretController {
     private boolean isViewSet;
     private final Map map;
     private final ShopController shopController;
-    private final java.util.Map<GridPosition, Turret> turrets;
+    private final java.util.Map<GridPosition, TurretManager> turrets;
     private final Semaphore semaphore;
     private final int tileSize;
+    private final EnemyController enemyController;
+    private final GameManager gameManager;
 
     /**
      * 
      * @param map
      * @param shopController
      * @param semaphore
+     * @param enemyController
+     * @param gameManager
      */
-    public TurretControllerImpl(final Map map, final ShopController shopController, final Semaphore semaphore) {
+    public TurretControllerImpl(final Map map, final ShopController shopController, final Semaphore semaphore, final EnemyController enemyController, final GameManager gameManager) {
         this.map = map;
         this.shopController = shopController;
         this.semaphore = semaphore;
         this.turrets = new HashMap<>();
         this.tileSize = ImgTileSize.getTileSize();
+        this.enemyController = enemyController;
+        this.gameManager = gameManager;
 //        turrets.put(new GridPosition(4, 4), new TurretImpl(0, new PositionConverter(ImgTileSize.getTileSize()).convertToPosition(new GridPosition(4, 4)), 100, 0, 0, 0, 0)); // test
 //        turrets.put(new GridPosition(11, 8), new TurretImpl(0, new PositionConverter(ImgTileSize.getTileSize()).convertToPosition(new GridPosition(11, 8)), 100, 0, 0, 0, 0)); // test rotation
     }
@@ -60,9 +70,9 @@ public class TurretControllerImpl implements TurretController {
             final Optional<Turret> t = shopController.buy();
             if (t.isPresent()) {
                 semaphore.acquireUninterruptibly();
-                final Turret newTurret = t.get().getClone();
-                newTurret.setPosition(new PositionConverter(this.tileSize).convertToPosition(gpos));
-                turrets.put(new GridPosition(gpos), newTurret);
+                final TurretManager newTurretManager = new TurretManagerImpl(t.get().getClone(), this.enemyController, this.gameManager);
+                newTurretManager.getTurret().setPosition(new PositionConverter(this.tileSize).convertToPosition(gpos));
+                turrets.put(new GridPosition(gpos), newTurretManager);
                 semaphore.release();
             }
         }
@@ -75,7 +85,7 @@ public class TurretControllerImpl implements TurretController {
 
     @Override
     public Iterator<Entry<GridPosition, Turret>> getTurretsIterator() {
-        return turrets.entrySet().iterator();
+        return turrets.entrySet().stream().map(e -> java.util.Map.entry(e.getKey(), e.getValue().getTurret())).iterator();
     }
 
     @Override
