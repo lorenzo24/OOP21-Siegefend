7e578f5310e519c7f5a2cfbb8596f706c06ecce6
Lorenzo <lorenzo.gessi2@gmail.com>
2022-04-19 18:13:06 +0200

    Add some tests for turret

---
+++ src/tests/TestTurret.java
@@ -0,0 +1,57 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import sgf.helpers.TurretsLoader;
+import sgf.helpers.TurretsLoaderImpl;
+import sgf.model.map.Position;
+import sgf.model.turret.Turret;
+import sgf.model.turret.TurretImpl;
+
+/**
+ * Testing class for turrets.
+ */
+public class TestTurret {
+
+    private static final int TURRET0_PRICE  = 200;
+    private static final int TURRET1_PRICE  = 250;
+    private static final int TURRET2_PRICE  = 400;
+    private static final Position POSITION  = new Position(3, 3);
+    private final TurretsLoader turretLoader = new TurretsLoaderImpl();
+    private final Turret turret0 = this.turretLoader.getTurrets().get(0);
+    private final Turret turret1 = this.turretLoader.getTurrets().get(1);
+    private final Turret turret2 = this.turretLoader.getTurrets().get(2);
+
+    /**
+     * Checks if the price of the turrets is correct.
+     */
+    @Test
+    public void priceTests() {
+        assertEquals(turret0.getPrice(), TURRET0_PRICE);
+        assertEquals(turret1.getPrice(), TURRET1_PRICE);
+        assertEquals(turret2.getPrice(), TURRET2_PRICE);
+    }
+
+    /**
+     * Checks if the initial position of the turret is correct.
+     */
+    @Test
+    public void initialPositionTests() {
+        assertNull(turret0.getPosition());
+        assertNull(turret1.getPosition());
+        assertNull(turret2.getPosition());
+    }
+
+    /**
+     * Tests if the setPosition() method works correctly.
+     */
+    @Test
+    public void setPositionTests() {
+        turret0.setPosition(POSITION);     
+    }
+}
