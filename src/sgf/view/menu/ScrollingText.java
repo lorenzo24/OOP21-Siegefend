385daae38e4cdc013c8d975cd0ae8ac086c9b383
Lorenzo <lorenzo.gessi2@gmail.com>
2022-04-19 16:48:50 +0200

    Add comments, Create timer as a local variable, Change value of TEXT_Y, Remove useless comment

--- src/sgf/view/menu/ScrollingText.java
+++ src/sgf/view/menu/ScrollingText.java
@@ -12,23 +12,24 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 /**
- * 
- * 
- *
+ * Shows a text that scrolls endlessly (end credits roll).
  */
 public class ScrollingText extends JPanel implements ActionListener {
 
     private static final long serialVersionUID = -4619185403316178271L;
     private static final String BACKGROUND_COLOR = "#293132", TEXT_COLOR = "#F7F9F9";
-    private static final Font TEXT_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 25); // OG: 25
-    private static final int TEXT_Y = 400;
-    private final Timer creditTimer;
+    private static final Font TEXT_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 25);
+    private static final int TEXT_Y = 660;
     private final String text;
     private int textY;
 
+    /**
+     * Basic Constructor.
+     * @param text the text we want to display
+     */
     public ScrollingText(final String text) {
         this.setBackground(Color.decode(BACKGROUND_COLOR));
-        creditTimer = new Timer(5, this);
+        final Timer creditTimer = new Timer(5, this);
         textY = TEXT_Y;
 
         this.text = text;
@@ -37,10 +38,11 @@ public class ScrollingText extends JPanel implements ActionListener {
 
     }
 
+    @Override
     public void paintComponent(final Graphics g) {
         super.paintComponent(g);
 
-        final Graphics2D g2d = (Graphics2D)g;
+        final Graphics2D g2d = (Graphics2D) g;
 
         g2d.setFont(TEXT_FONT);
         g2d.setColor(Color.decode(TEXT_COLOR));
@@ -57,17 +59,14 @@ public class ScrollingText extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-
         textY--;
-        if (textY < -660) {
+        if (textY < -TEXT_Y) {
             restart();
         }
-
         repaint();
     }
 
     private void restart() {
         textY = TEXT_Y;
     }
-
 }
