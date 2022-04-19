7c6a45f722ad9e9466f73529d15386936ce9b401
Lorenzo <lorenzo.gessi2@gmail.com>
2022-04-19 21:32:43 +0200

    Add comment, Remove useless comments

--- src/sgf/view/menu/AbstractMenuView.java
+++ src/sgf/view/menu/AbstractMenuView.java
@@ -3,15 +3,8 @@ package sgf.view.menu;
 import javax.swing.JPanel;
 
 /**
- * 
- * 
- *
+ * Abstract class for the view of the menu based on {@link JPanel}.
  */
 public abstract class AbstractMenuView extends JPanel implements MenuView {
-
-    /**
-     * 
-     */
     private static final long serialVersionUID = 7006656449197043734L;
-
 }
